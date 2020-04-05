b243782ab6f8ca2330b3335185873c3f82fcdba5
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-04-05 12:48:07 -0400

    Slightly less garbage checkpoint

--- index.js
+++ index.js
@@ -1,58 +1,66 @@
 const validateLineup = (lineup) => {
 
   // if salary > 45.000 - FALSE
-  // return calculateSalary(lineup) // Function working!
-
-
-  // if players 'OF' < 3 -- FALSE
-  // if players dont have ALL --- 'P', 'C', '1B', '2B', '3B', 'SS' -- False
-
+  //return calculateSalary(lineup) // Function working!
 
   // if calculateTeamPlayers > 2 -- FALSE
-  //return calculateTeamPlayers(lineup) <= 2 //WORKSSSSSS
+  //return calculateNumberPlayers(lineup.map((lineup) => lineup.teamId)) <= 2
 
   // if calculateGamePlayers > 3 -- FALSE
-  return calculateGamePlayers(lineup) <= 3
+  //return calculateNumberPlayers(lineup.map((lineup) => lineup.gameId)) <= 3
+
+  // MAP FOR POSITIONS
+  const gamePositions = lineup.map((lineup) => lineup.position)
+
+  console.log(gamePositions)
+
+  const totalOF = getOccurrence(gamePositions, 'OF') // need 3
+  console.log(totalOF === 3)
+
+  const totalP = getOccurrence(gamePositions, 'P') //need 1
+  console.log(totalP === 1)
+
+  const totalC = getOccurrence(gamePositions, 'C') //need 1
+  console.log(totalC === 1)
+
+  const total1B = getOccurrence(gamePositions, '1B') //need 1
+  console.log(total1B === 1)
+
+  const total2B = getOccurrence(gamePositions, '2B') //need 1
+  console.log(total2B === 1)
+
+  const total3B = getOccurrence(gamePositions, '3B') //need 1
+  console.log(total3B === 1)
+
+  const totalSS = getOccurrence(gamePositions, 'SS') //need 1
+  console.log(totalSS === 1)
+
+  // Need one of each of these
+  return checkIndividualPositions(gamePositions)
 }
 
-// WHRE DID WE DO SUM ALL WITH REDUCEEEEEE THIS IS SUM ALL!
+// This would be cleaner than seperate variables, but doesnt work....!!!!
+const checkIndividualPositions = (gamePositions) => {
+  const individualPositions = ['P', 'C', '1B', '2B', '3B', 'SS']
+
+  for (let i = 0; i < individualPositions.length; i++) {
+    console.log(individualPositions[i])
+    console.log(getOccurrence(gamePositions, individualPositions[i]))
+    console.log(getOccurrence(gamePositions, individualPositions[i]) === 1)
+
+    return getOccurrence(gamePositions, individualPositions[i]) === 1
+    // This only returns the first item...
+  }
+}
+
 const calculateSalary = (lineup) => {
-  let totalSalary = 0
-
-  for (let i = 0; i < lineup.length; i++) {
-    totalSalary += lineup[i].salary
-  }
-
-  return totalSalary < 45000
-
+  return lineup.map((lineup) => lineup.salary)
+    .reduce((total, salary) => total + salary, 0) < 45000
 }
 
-const calculateGamePlayers = (lineup) => {
-  let gamePlayers = []
-  // THIS IS BAD FIX IT!!!!
-
-  // Put all GAME players in array.... Destructuring??????
-  for (let i = 0; i < lineup.length; i++) {
-    gamePlayers.push(lineup[i].gameId)
-  }
-  // Check that there are 2 or less plauers on a team.... Howww???
-  for (let i = 0; i < gamePlayers.length; i++) {
-    return getOccurrence(gamePlayers, gamePlayers[i])
-  }
-}
-
-
-const calculateTeamPlayers = (lineup) => {
-  let teamPlayers = []
-  // THIS IS BAD FIX IT!!!!
-
-  // Put all team players in array.... Destructuring??????
-  for (let i = 0; i < lineup.length; i++) {
-    teamPlayers.push(lineup[i].teamId)
-  }
-  // Check number of team players.... Howww to merge???
-  for (let i = 0; i < teamPlayers.length; i++) {
-    return getOccurrence(teamPlayers, teamPlayers[i])
+const calculateNumberPlayers = (players) => {
+  for (let i = 0; i < players.length; i++) {
+    return getOccurrence(players, players[i])
   }
 }
 
@@ -60,13 +68,4 @@ const getOccurrence = (array, value) => {
   return array.filter((v) => (v === value)).length
 }
 
-
-/*
-## Lineup Rules
-1) The total salary of all players in a lineup may not exceed $45,000
-2) Lineups may not contain more than 2 players from a single team
-3) Lineups may not contain more than 3 players from a single game
-4) Lineups must contain exactly 3 players with the position of 'OF' and must also contain exactly 1 player from each of the following positions: 'P', 'C', '1B', '2B', '3B', 'SS'
-
-*/
 module.exports = validateLineup
