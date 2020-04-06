f8116d4a92616de5664050dc2ad3992915341c0a
Max Sauber <maxsauber@Maxs-Air.fios-router.home>
2020-04-06 15:51:58 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 const validateLineup = (lineup) => {
-  const positions = lineup.map((lineup) => lineup.position)
+  const allPositions = lineup.map((lineup) => lineup.position)
   const singlePositions = ['P', 'C', '1B', '2B', '3B', 'SS']
   const multiPositions = ['OF']
 
   return calculateSalary(lineup) <= 45000 &&
     evalPlayers(lineup.map((lineup) => lineup.teamId), 2) &&
     evalPlayers(lineup.map((lineup) => lineup.gameId), 3) &&
-    evalPositions(positions, singlePositions, 1) &&
-    evalPositions(positions, multiPositions, 3)
+    evalPositions(allPositions, singlePositions, 1) &&
+    evalPositions(allPositions, multiPositions, 3)
 }
 
 const calculateSalary = (lineup) => lineup.map((lineup) => lineup.salary).reduce((total, salary) => total + salary, 0)
 
 const evalPlayers = (players, max) => {
+  // players.forEach()
+
+  //players.forEach(getOccurrence(players, players[i]) > max return false)
+
   for (let i = 0; i < players.length; i++) {
-    if (getOccurrence(players, players[i]) > max) {
-      return false
-    }
+    if (getOccurrence(players, players[i]) > max) return false
   }
 
   return true
@@ -24,9 +26,7 @@ const evalPlayers = (players, max) => {
 
 const evalPositions = (positions, requiredPositions, max) => {
   for (let i = 0; i < requiredPositions.length; i++) {
-    if (getOccurrence(positions, requiredPositions[i]) !== max) {
-      return false
-    }
+    if (getOccurrence(positions, requiredPositions[i]) !== max) return false
   }
 
   return true
